e4cb213f987ddea12995df618492e4825929a664
JithuPrijith <prijith@storebase.in>
2023-02-02 07:46:57 +0530

    minor changes

--- cliTask/index.js
+++ cliTask/index.js
@@ -3,20 +3,18 @@ const fs = require("fs");
 
 const src = process.argv[2];
 const dest = process.argv[3];
-console.log(src,dest);
+console.log(src, dest);
 
-
-
-function copyFile(src,dest,callback){
-    fs.cp(src,dest,(err) => {
-        if(err){
-            console.log(err);
-            return err;
-        }
-        callback();
-    });
+function copyFile(src, dest, callback) {
+  fs.cp(src, dest, (err) => {
+    if (err) {
+      console.log(err);
+      return err;
+    }
+    callback();
+  });
 }
 
-copyFile(src,dest,() => {
-    console.log("done");
+copyFile(src, dest, () => {
+  console.log("done");
 });
